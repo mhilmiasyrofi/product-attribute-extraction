4e4a26f36c0c1f65a57ff0096342d17262455e5e
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-02-26 14:30:53 +0800

    chore: script to fine-tune

--- fine-tune.sh
+++ fine-tune.sh
@@ -1,5 +1,2 @@
-python3 preprocessEnimex.py
-python3 convertEnimex2Stanford.py
-python3 convertStanford2BIO.py
-python3 filterBIO.py
-python3 fineTune.py
+python3 preprocess.py
+python3 fine_tune.py
